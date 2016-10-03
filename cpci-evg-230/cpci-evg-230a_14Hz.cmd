ce915e9e97d4ebd97bd469e028383d679ab86f15
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-10-03 18:27:29 +0200

    update EVG address

--- cpci-evg-230/cpci-evg-230a_14Hz.cmd
+++ cpci-evg-230/cpci-evg-230a_14Hz.cmd
@@ -1,13 +1,13 @@
 # This file should be used with EPICS base 3.14.12.5 and mrfioc2 2.1.0
 # With current EEE 1.8.2, the proper command is 
-# $ iocsh -3.14.12.5 cpci-evg-230_0.cmd
+# $ iocsh -3.14.12.5 cpci-evg-230a_14Hz.cmd
 
 require mrfioc2,2.1.0
 
 epicsEnvSet("SYS"             "ICS-CPCIEVG-230")
 epicsEnvSet("EVG"             "EVG0")
 epicsEnvSet("EVG_PCIBUS"      "0x16")
-epicsEnvSet("EVG_PCIDEVICE"   "0x09")
+epicsEnvSet("EVG_PCIDEVICE"   "0x0e")
 epicsEnvSet("EVG_PCIFUNCTION" "0x0")
 mrmEvgSetupPCI($(EVG), $(EVG_PCIBUS), $(EVG_PCIDEVICE), $(EVG_PCIFUNCTION))
 
