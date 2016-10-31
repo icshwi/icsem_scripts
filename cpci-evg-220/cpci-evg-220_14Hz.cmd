dc0c863548e195ba4b3dfe4baf8ec9faa2a0b361
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-10-31 17:03:23 +0100

    add 220 startup script

--- cpci-evg-220/cpci-evg-220_14Hz.cmd
+++ cpci-evg-220/cpci-evg-220_14Hz.cmd
@@ -1,17 +1,20 @@
 # This file should be used with EPICS base 3.15.4 and mrfioc2 2.7.13
 # With current EEE 1.8.2, the proper command is 
 # $ iocsh -3.14.12.5 cpci-evg-220_14Hz.cmd
+# iocsh -3.14.12.5 cpci-evg-220/cpci-evg-220_14Hz.cmd 
 
-require mrfioc2,2.1.0
+require mrfioc2,iocuser
 
-epicsEnvSet("SYS"             "ICS-cPCI")
-epicsEnvSet("EVG"             "EVG220")
-epicsEnvSet("EVG_PCIBUS"      "0x01")
-epicsEnvSet("EVG_PCIDEVICE"   "0x09")
-epicsEnvSet("EVG_PCIFUNCTION" "0x0")
-mrmEvgSetupPCI($(EVG), $(EVG_PCIBUS), $(EVG_PCIDEVICE), $(EVG_PCIFUNCTION))
 
-dbLoadRecords("evg-cpci.db", "EVG=$(EVG), SYS=$(SYS)")
+epicsEnvSet("SYS"       "ICS-CPCI")
+epicsEnvSet("EVG"       "EVG220")
+epicsEnvSet("EVG_BUS"   "0x16")
+epicsEnvSet("EVG_DEV"   "0x0e")
+epicsEnvSet("EVG_FUNC"  "0x0")
+mrmEvgSetupPCI($(EVG), $(EVG_BUS), $(EVG_DEV), $(EVG_FUNC))
+# --------------------------------------------------------
+# dbLoadRecords example
+dbLoadRecords("evg-cpci.db", "DEVICE=$(EVG), SYS=$(SYS)")
 
 
 iocInit
@@ -28,3 +31,12 @@ dbpf $(SYS)-$(EVG):Mxc0-Frequency-SP 14
 dbpf $(SYS)-$(EVG):Mxc0-TrigSrc0-SP 1
 
 dbpf $(SYS)-$(EVG):SoftEvt-Enable-Sel 1
+
+
+mrmEvgSoftTime("$(EVG)")
+sleep(5)
+dbpf $(SYS)-$(EVG):SyncTimestamp-Cmd 1
+
+
+
+dbl > cpci-evg-220/cpci-evg-220.pvlist
