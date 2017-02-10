352d83372abb034e0a8d361ca1918d57c33c658a
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-02-10 17:30:15 +0100

    add evrs with events

---
+++ mtca-evr-300/test_evr-mtca-300-evg.cmd
@@ -0,0 +1,26 @@
+#  -*- mode: epics -*-
+# $ iocsh test_evr-mtca-300.cmd
+
+require mrfioc2,2.7.13
+
+epicsEnvSet(       "SYS"     "MTCA424")
+
+epicsEnvSet(       "EVR"        "EVR0")
+epicsEnvSet(   "EVR_BUS"        "0x10")
+epicsEnvSet(   "EVR_DEV"        "0x00")
+epicsEnvSet(  "EVR_FUNC"         "0x0")
+epicsEnvSet("EVR_DOMAIN"      "0x0000")
+mrmEvrSetupPCI($(EVR), $(EVR_DOMAIN), $(EVR_BUS), $(EVR_DEV), $(EVR_FUNC))
+
+dbLoadRecords("evr-mtca-300.db", "DEVICE=$(EVR), SYS=$(SYS), Link-Clk-SP=88.0525")
+
+
+#Generate trigger signals
+dbLoadRecords("evr-softEvent.template", "DEVICE=$(EVR), SYS=$(SYS), CODE=14, EVT=14")
+dbLoadRecords("evr-pulserMap.template", "DEVICE=$(EVR), SYS=$(SYS), PID=0, F=Trig, ID=0, EVT=14")
+
+
+
+iocInit
+
+#
